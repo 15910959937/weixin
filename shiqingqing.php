630c85567b44d5639a4561dd87116fdc9a62f414
Your Name <you@example.com>
2016-08-24 19:34:29 +0800

    时庆庆1

--- shiqingqing.php
+++ shiqingqing.php
@@ -1,39 +1,45 @@
 <?php
 /**
-  * wechat php test
-  */
-
+ * wechat php test
+ */
+header('content-type:text');
+//define your token
 define("TOKEN", "weixin");
 $wechatObj = new wechatCallbackapiTest();
+//如果成功建立，该方法注释掉
 //$wechatObj->valid();
 $wechatObj->responseMsg();
-
 class wechatCallbackapiTest
 {
-	public function valid()
+    public function valid()
     {
         $echoStr = $_GET["echostr"];
+
         //valid signature , option
+        //建立连接成功后直接退出
         if($this->checkSignature()){
-        	echo $echoStr;
-        	exit;
+            echo $echoStr;
+            exit;
         }
     }
 
-    public function responseMsg(){
-		//get post data, May be due to the different environments
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-      	//extract post data
-		if (!empty($postStr)){
-                /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
-                   the best way is to check the validity of xml by yourself */
-                libxml_disable_entity_loader(true);
-              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $time = time();
-                $textTpl = "<xml>
+    public function responseMsg()
+    {
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+        //extract post data
+        if (!empty($postStr)){
+            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+               the best way is to check the validity of xml by yourself */
+            libxml_disable_entity_loader(true);
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $msgtype = $postObj->MsgType;
+            $textTpl = "<xml>
 							<ToUserName><![CDATA[%s]]></ToUserName>
 							<FromUserName><![CDATA[%s]]></FromUserName>
 							<CreateTime>%s</CreateTime>
@@ -41,40 +47,85 @@ class wechatCallbackapiTest
 							<Content><![CDATA[%s]]></Content>
 							<FuncFlag>0</FuncFlag>
 							</xml>";
-				if(!empty( $keyword )){
-              		$msgType = "text";
-                	$contentStr = "Welcome to wechat world!";
-                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
+            $musTpl = "<xml>
+                            <ToUserName><![CDATA[%s]]></ToUserName>
+                            <FromUserName><![CDATA[%s]]></FromUserName>
+                            <CreateTime>%s</CreateTime>
+                            <MsgType><![CDATA[%s]]></MsgType>
+                            <Music>
+                            <Title><![CDATA[%s]]></Title>
+                            <Description><![CDATA[%s]]></Description>
+                            <MusicUrl><![CDATA[%s]]></MusicUrl>
+                            <HQMusicUrl><![CDATA[%s]]></HQMusicUrl>
+                            </Music>
+                            </xml>";
+            if($msgtype == "text"){
+                if(!empty( $keyword ))
+                {
+                    if($keyword == "音乐"){
+                        $msgType = "music";
+                        $title = "刘美君我爱你";
+                        $description = "原声大碟，非常动听";
+                        $mus_url = "http://yyzljg.com/wechat/music.mp3";
+                        $hq = "http://yyzljg.com/wechat/music.mp3";
+                        $resultStr = sprintf($musTpl, $fromUsername, $toUsername, $time, $msgType, $title, $description, $mus_url, $hq);
+                        echo $resultStr;
+                    }elseif($keyword == "图文"){
+                        $msgType = "image";
+                        $contentStr = "";
+                        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                        echo $resultStr;
+                    }
+
                 }else{
-                	echo "Input something...";
+                    echo "Input something...";
                 }
+            }elseif($msgtype == "image"){
+                $msgType = "text";
+                $contentStr = "嘿嘿嘿！你这么丑想吓唬谁";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }elseif($msgtype == "voice"){
+                $msgType = "text";
+                $contentStr = "嘿嘿嘿！耳背大点声！";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }elseif($msgtype =="location"){
+                $msgType = "text";
+                $contentStr = "嘿嘿嘿！已经被我的锁定了";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }
         }else {
-        	echo "";
-        	exit;
+            echo "";
+            exit;
         }
     }
 
-	private function checkSignature(){
+    private function checkSignature()
+    {
         // you must define TOKEN by yourself
         if (!defined("TOKEN")) {
             throw new Exception('TOKEN is not defined!');
         }
+
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
-		$token = TOKEN;
-		$tmpArr = array($token, $timestamp, $nonce);
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
         // use SORT_STRING rule
-		sort($tmpArr, SORT_STRING);
-		$tmpStr = implode( $tmpArr );
-		$tmpStr = sha1( $tmpStr );
-		if( $tmpStr == $signature ){
-			return true;
-		}else{
-			return false;
-		}
-	}
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
 
 ?>
